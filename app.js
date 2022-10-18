17c0fe787452648aa5866f9f1d6fc4c07bc81a92
malecki-se <malecki.se@gmail.com>
2022-10-18 20:37:19 +0000

    test duplication

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ const b = {
     b: 2,
     c: 3
 }
+
+function foo() {
+    const a = 0;
+    for (let i = 0; i++; i < 10) {
+        a++
+    }
+    return a;
+}
+
+function bar() {
+    const a = 0;
+    for (let i = 0; i++; i < 10) {
+        a++
+    }
+    return a;
+}
