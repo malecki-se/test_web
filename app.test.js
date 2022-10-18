4f30594cf55365230cab66aa76075a1d58a7dc69
malecki-se <malecki.se@gmail.com>
2022-10-18 21:54:53 +0200

    update test

--- app.test.js
+++ app.test.js
@@ -1,7 +1,11 @@
 test('shows some item', () => {
+    const a = 1;
+    a.map((a) => 2)
     expect(213)
 })
 
 test('shows some item', () => {
-    expect(231)
+    const a = 1;
+    a.map((a) => 2)
+    expect(213)
 })
